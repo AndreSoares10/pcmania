a70915348cd0d58035cc453cbb9547d113e3e3b6
AndreSoares10 <andre.barbosa@gec.inatel.br>
2025-04-03 21:46:48 -0300

    Create Cliente.java

---
+++ Cliente.java
@@ -0,0 +1,11 @@
+public class Cliente {
+ 
+    String nome;
+    int matricula;
+ 
+    // Construtor para os atributos do Cliente
+    Cliente(String nome, int matricula) {
+        this.nome = nome;
+        this.matricula = matricula;
+    }
+}
